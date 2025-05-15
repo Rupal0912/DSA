5a153ce9bade168b0aca975e91174d10b8689cdc
Rupal0912 <rupaltripathi464@gmail.com>
2025-05-15 16:53:05 +0530

    sliding window maxsubaaray k sum with bruteforce done

---
+++ Arrays/longestsubarraywithksum.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+// brute force
+
+int main(){
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int k;
+    cout << "Enter the value of k: ";
+    cin >> k;
+
+   int length = 0;
+    for(int i=0;i<n;i++){
+        int sum = 0;
+        for(int j=i;j<n;j++){
+            sum += arr[j];
+            if(sum == k){
+                length = max(length, j-i+1);
+            }
+        }
+        
+    }
+    cout<<length<<endl; 
+    return 0;
+}
